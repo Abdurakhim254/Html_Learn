1be14baf28b10a942ef841cdcf90ee06c84eeff6
Abdurakxim254 <toxirmalk@gmail.com>
2025-02-11 17:25:06 +0500

    All files changed

--- main.js
+++ main.js
@@ -1,35 +1,100 @@
 const box=document.querySelector('.box')
 
+const button=document.querySelector('.buttons')
+
+const input=document.querySelector('.inputs')
+
 const render=(data)=>{
     box.innerHTML=data.map((item)=>
     `
     <div>
-    <h1>${item}</h1>
-    <button class="delete" id="${Date.now()}">Delete</button>
-    <button class="edit" id="${Date.now()}">Edit</button>
+    <h1>${item.name}</h1>
+    <button class="delete" id="${item.id}">Delete</button>
+    <button class="edit" id="${item.id}">Edit</button>
     </div>
     `
     ).join("")
 }
 
 
-const delbtns=document.querySelectorAll('.delete')
-const wditbtns=document.querySelectorAll('.edit')
-
-
 const getData=async()=>{
     try {
-        const res=await fetch("https://jsonplaceholder.typicode.com/users")
+        const res=await fetch("http://localhost:3600/user",{
+            method:"GET"
+        })
         
         const data=await res.json()
 
-        console.log(data);
-        
         render(data)
     } catch (error) {
         alert(error.message)
     }
 }
+
+
+
+const deletedata=async(id)=>{
+    try {
+        const res=await fetch(`http://localhost:3600/user/${id}`,{
+            method:"DELETE"
+        })
+                
+    } catch (error) {
+        alert(error.message)
+    }
+}
+
+const updatedata=async(data)=>{
+    try {
+        const res=await fetch(`http://localhost:3600/user/${data.id}`,{
+            method:"PUT",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body:JSON.stringify({name:data.name,id:data.id})
+        })
+        
+        
+    } catch (error) {
+        alert(error.message)
+    }
+}
+
+const createdata=async(data)=>{
+    try {
+        console.log(data);
+        const res=await fetch("http://localhost:3600/user",{
+            method:"POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body:JSON.stringify({name:data})
+        })
+        return res
+    } catch (error) {
+        alert(error.message)
+    }
+}
+
+
+button.addEventListener('click',(e)=>{
+    e.preventDefault()
+    createdata(input.value)
+})
+
+
+box.addEventListener('click',async(e)=>{
+    e.preventDefault()
+    if(e.target.className=='delete'){
+        deletedata(e.target.id)
+    }else if(e.target.className=='edit'){
+        const name=prompt("Type something")
+        const id=e.target.id
+        updatedata({id,name})
+    }
+})
+
+
 
 
 getData()
