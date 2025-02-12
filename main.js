4c7927a856c13c3ef24158c675e3ebf0acbe1205
Abdurakxim254 <toxirmalk@gmail.com>
2025-02-12 07:27:05 +0500

    Crud APi is ready

--- main.js
+++ main.js
@@ -2,33 +2,30 @@
 
 // import { forIn } from "lodash"
 
+const getItem = (value) => {
+  try {
+    const localdatas = localStorage.getItem(value);
+    return JSON.parse(localdatas);
+  } catch (error) {
+    return undefined;
+  }
+};
 
-const getItem=(value)=>{
-    try {
-        const localdatas=localStorage.getItem(value)
-        return JSON.parse(localdatas)
-    } catch (error) {
-        return undefined
-    }
-}
+const setItem = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
 
+const box = document.querySelector(".box");
 
-const setItem=(key,value)=>{
-    localStorage.setItem(key,JSON.stringify(value))
-}
+const button = document.querySelector(".buttons");
 
+const input = document.querySelector(".inputs");
 
-
-
-const box=document.querySelector('.box')
-
-const button=document.querySelector('.buttons')
-
-const input=document.querySelector('.inputs')
-
-const render=(data)=>{
-    box.innerHTML=data.map((item)=>
-    `
+const render = (data) => {
+  box.innerHTML = data
+    .map(
+      (item) =>
+        `
     <div>
     <h1>${item.name}</h1>
     <button class="delete" id="${item.id}">Delete</button>
@@ -36,115 +33,127 @@ const render=(data)=>{
     <button class="save_local" id="${item.id}">Save btn</button>
     </div>
     `
-    ).join("")
-}
+    )
+    .join("");
+};
 
+const getData = async () => {
+  try {
+    const res = await fetch("http://localhost:3600/user", {
+      method: "GET",
+    });
 
-const getData=async()=>{
-    try {
-        const res=await fetch("http://localhost:3600/user",{
-            method:"GET"
-        })
-        
-        const data=await res.json()
-        render(data)
-    } catch (error) {
-        alert(error.message)
-    }
-}
+    const data = await res.json();
+    render(data);
+  } catch (error) {
+    alert(error.message);
+  }
+};
 
+const getoneData = async (id) => {
+  try {
+    const res = await fetch(`http://localhost:3600/user/${id}`, {
+      method: "GET",
+    });
 
-const getoneData=async(id)=>{
-    try {
-        const res=await fetch(`http://localhost:3600/user/${id}`,{
-            method:"GET"
-        })
-        
-        const data=await res.json()
-        return data
-    } catch (error) {
-        alert(error.message)
-    }
-}
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    alert(error.message);
+  }
+};
 
+const deletedata = async (id) => {
+  try {
+    const res = await fetch(`http://localhost:3600/user/${id}`, {
+      method: "DELETE",
+    });
+  } catch (error) {
+    alert(error.message);
+  }
+};
 
-const deletedata=async(id)=>{
-    try {
-        const res=await fetch(`http://localhost:3600/user/${id}`,{
-            method:"DELETE"
-        })
-                
-    } catch (error) {
-        alert(error.message)
-    }
-}
+const updatedata = async (data) => {
+  try {
+    const res = await fetch(`http://localhost:3600/user/${data.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: data.name, id: data.id }),
+    });
+  } catch (error) {
+    alert(error.message);
+  }
+};
 
-const updatedata=async(data)=>{
-    try {
-        const res=await fetch(`http://localhost:3600/user/${data.id}`,{
-            method:"PUT",
-            headers:{
-                "Content-Type": "application/json"
-            },
-            body:JSON.stringify({name:data.name,id:data.id})
-        })
-        
-        
-    } catch (error) {
-        alert(error.message)
-    }
-}
+const createdata = async (data) => {
+  try {
+    const res = await fetch("http://localhost:3600/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: data }),
+    });
+  } catch (error) {
+    alert(error.message);
+  }
+};
 
-const createdata=async(data)=>{
-    try {
-        const res=await fetch("http://localhost:3600/user",{
-            method:"POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body:JSON.stringify({name:data})
-        })
+button.addEventListener("click", (e) => {
+  createdata(input.value);
+  e.preventDefault();
+});
 
-        
+box.addEventListener("click", async (e) => {
+  e.preventDefault();
+  const classlists = ["delete", "edit", "save_local"];
 
-    } catch (error) {
-        alert(error.message)
-    }
-}
+  const id = e.target.id;
 
+  const localdatas = getItem("user") || [];
 
-button.addEventListener('click',(e)=>{
-    createdata(input.value)
-    e.preventDefault()
-})
+  const data = await getoneData(id);
 
+  if (classlists.includes(e.target.className)) {
+    if (e.target.className == "delete") {
 
-box.addEventListener('click',async(e)=>{
-    e.preventDefault()
-    const classlists=['delete','edit','save_local']
+      deletedata(id);
 
-    const id=e.target.id
-    if(classlists.includes(e.target.className)){
-         if(e.target.className=='delete'){
-        deletedata(e.target.id)
-    }else if(e.target.className=='edit'){
-        const name=prompt("Type something")
-        const id=e.target.id
-        updatedata({id,name})   
-    }else{
-        const localdatas=getItem('user')||[]
+      let newdatas=[]
 
-        const data=await getoneData(id)
-        
-        const onedata=localdatas.find((item)=>item.id==id)
-
-        if(!onedata){
-            setItem('user',[...localdatas,data])
+      for(let i=0;i<localdatas.length;i++){
+        if(localdatas[i].id!=id){
+          newdatas.push(localdatas[i])
         }
+      }
+      
+      setItem("user",[...newdatas])
 
+    } else if (e.target.className == "edit") {
+      const name = prompt("Type something");    
+
+      for (let  i= 0; i< localdatas.length; i++) {
+          if(localdatas[i].id==id){
+            localdatas[i].name=name
+          }
+      }
+
+
+      updatedata({ id, name });
+
+      setItem("user",[...localdatas])
+
+    } else {
+
+
+      const onedata = localdatas.find((item) => item.id == id);
+      if (!onedata) {
+        setItem("user", [...localdatas, data]);
+      }
     }
-    }
+  }
+});
 
-})
-
-getData()
+getData();
