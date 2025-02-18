dca5ed239a3bb59cce417e205002e4c663a6486d
Abdurakxim254 <toxirmalk@gmail.com>
2025-02-18 16:45:47 +0500

    Project is started

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,13 @@
-import { request } from './config/request'
-import React from 'react'
-import { Card } from './components/card'
-function App() {
-
-  const [users, Setusers] = React.useState([])
-
-  const [inputtext,SetCurrentText]=React.useState("")
-
-  const setData = () => {
-    request.get('/todos')
-    .then(res => Setusers(res.data))
-  }
-
-  const createData = (title) => {
-    request.post('/todos',{title,description:"hello"})
-     setData()
-  }
+import React from "react";
 
 
-  React.useEffect(setData,[])
-  return (
-    <>
-      <div>
-        <input type="text" placeholder='Enter Something' value={inputtext} onChange={(e) => SetCurrentText(e.target.value)} />
-        <button onClick={()=>{createData(inputtext.trim("")),SetCurrentText("")}}>Press</button>
-      </div>
-      {users.map(item => <Card title={item.title} key={item.id} id={item.id} setData={setData}/>)}
-    </>
-  )
+export const App = () => {
+    return (
+        <>
+            <h1>Salom</h1>
+        </>
+    )
 }
+
 
 export default App
