0874aa5b353b18419c852803c12b45b240e737cd
Abdurakxim254 <toxirmalk@gmail.com>
2025-02-20 13:51:09 +0500

    Some change

--- day-13/src/pages/delete-task.jsx
+++ day-13/src/pages/delete-task.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { todoAction } from "../context/todo-context/todo-reducer";
 import {TodoContextProvider } from "../context/todo-context/todo-context";
-import { useNavigate } from "react-router-dom";
 
 
 export const DeleteTask = () => {
 
     const {id}=useParams()
 
-    const navigate = useNavigate();
 
     const { dispatch } = React.useContext(TodoContextProvider);
 
@@ -23,8 +21,4 @@ export const DeleteTask = () => {
 
 
     return (<div>Task Deleted</div>);
-
-    setTimeout(() => {
-        navigate("/");
-    }, 2000);
 }
